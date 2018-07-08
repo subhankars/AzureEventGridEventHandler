941d120b8a9e61cd0839431474a0964ca6d23deb
Subhankar Sarkar <s.subhankar@gmail.com>
2018-07-08 22:13:21 +0530

    Change in function name

--- EventGridEventTrigger.AzureFunctionApp/EventGridEventHandler.cs
+++ EventGridEventTrigger.AzureFunctionApp/EventGridEventHandler.cs
@@ -10,7 +10,7 @@ namespace EventGridEventFunctionApp
 {
     public static class EventGridEventHandler
     {
-        [FunctionName("Function1")]
+        [FunctionName("EventGridEventHandler")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
